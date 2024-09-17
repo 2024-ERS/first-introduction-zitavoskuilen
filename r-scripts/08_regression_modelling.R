45e58c2846a1684c239cd73188f1683bfc1004fb
Zita Voskuilen <z.m.voskuilen@student.rug.nl>
2024-09-17 10:39:53 +0200

    completed the script

--- r-scripts/08_regression_modelling.R
+++ r-scripts/08_regression_modelling.R
@@ -151,17 +151,24 @@ anova(m4,m3)
 #### m3 is the preferred model in this case (outcome of model selection)
 # we tried to find the best model 
 
+## dinsdag 17 september 2024 
+
 # explore the consequences of a log transformation of y values
 x<-c(0,1,2,3,4,5)
-y<-c(1,10,100,1000,10000,100000)
+y<-c(0,0,100,5000,13900,100000)
 dat<-data.frame(x,y)
 dat
+
+# ecological response for instance 
 dat %>% ggplot(aes(x=x,y=y)) +
   geom_point(shape=16,size=5) +
   geom_line(size=1.2)
+
 dat %>% ggplot(aes(x=x,y=log10(y))) +
   geom_point(shape=16,size=3) +
-  geom_line(size=1.2)
+#  geom_line(size=1.2)
+  geom_smooth(method = "lm")
+
 dat %>% ggplot(aes(x=x,y=log(y))) + # use log with base number e
   geom_point(shape=16,size=3) +
   geom_line(size=1.2)
@@ -175,16 +182,29 @@ exp(1)
 log(exp(1))
 
 ### develop, test for significance and plot different models of increasing complexity 
-#  using  multiple regresssion, assuming a poisson distribution (so use a generalized linear model)
+#  using  multiple regression, assuming a poisson distribution (so use a generalized linear model)
 # Explore  how the abundance of Orchestia depends on elevation_m and year,  their potential interaction,
 # and a potential ecological optimum of Orchestia with respect to elevation_m
 # show the effect of elevation but now in a generalized linear model instead of linear model, using a log link function and a poisson distribution
+
+orchdat3
+
+p1
 
 
 #add the linear model to the plot
 # calculate the predicted value of m2 for every observation, add to the dataset as a variable as pred2
 # add the new predicted line to the previous plot p2, store as object p3 and show it
 
+m5 <- glm(CountSum ~ elevation_m, 
+          family = poisson(log),
+          data  = orchdat3)
+
+anova(m5, test = "Chisq")
+# elevation has a significant effect on the abundance of Orchestia
+
+orchdat3$pred5 <- predict(m5, type = "response")
+p1 + geom_line(data= orchdat3, aes(y = pred5), linewidth = 1.2)
 
 
 # now test and show  the effect of both elevation , elevation squared and year
@@ -194,6 +214,12 @@ log(exp(1))
 # calculate the predicted value of m2 for every observation, add to the dataset as a variable as pred2
 # add the new predicted line to the previous plot p2, store as object p3 and show it
 
+m6 <- glm(CountSum ~ elevation_m + I(elevation_m^2), 
+          family = poisson(log),
+          data  = orchdat3)
+
+orchdat3$pred6 <- predict(m6, type = "response")
+p1 + geom_line(data= orchdat3, aes(y = pred6), linewidth = 1.2)
 
 # better than the previous?
 
