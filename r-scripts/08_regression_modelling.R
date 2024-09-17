6533d4ccd68a23f2315d841397bc52542b2e05e0
Zita Voskuilen <z.m.voskuilen@student.rug.nl>
2024-09-17 10:40:18 +0200

    made it

--- r-scripts/08_regression_modelling.R
+++ r-scripts/08_regression_modelling.R
@@ -227,12 +227,18 @@ p1 + geom_line(data= orchdat3, aes(y = pred6), linewidth = 1.2)
 # add the interaction to the model: elevation + elevation ^2 + year + elevation*year
 # now test and show  the effect of both elevation + year
 
+m7 <- glm(CountSum ~ elevation_m + I(elevation_m^2) + factor(year) + elevation_m*factor(year), 
+          family = poisson(log),
+          data  = orchdat3)
+
+orchdat3$pred7 <- predict(m7, type = "response")
+p1 + geom_line(data= orchdat3, aes(y = pred7), linewidth = 1.2)
+
+anova(m6, m7,test = "Chisq")
 
 #add the  model to the plot
 # calculate the predicted value of m2 for every observation, add to the dataset as a variable as pred2
 # add the new predicted line to the previous plot p2, store as object p3 and show it
-
-
 
 
 
